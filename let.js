245e86997c3a9abafdf471c3aa6ecf39618b9921
syedomer17 <syedomeali2006@gmail.com>
2024-10-29 15:53:31 +0530

    practice

---
+++ let.js
@@ -0,0 +1,19 @@
+// var 
+
+let a = 10;
+console.log(a);
+// undefined 
+let b;
+console.log(b);
+//redeclare 
+// let x = 10;
+// let x = 20;
+// let x =30;
+// console.log(x);
+// as you can see it can't be redeclare but it can be reassign 
+
+// reassign
+let y = 13;
+y = 16;
+y = 22;
+console.log(y);
