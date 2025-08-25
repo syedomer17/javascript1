c91bab48d1546f5c986717e427f54ef178a6cb82
syedomer17 <syedomerali2006@gmail.com>
2025-08-25 17:17:46 +0530

    Auto-commit on Mon Aug 25 17:17:46 IST 2025

---
+++ javascript/const.js
@@ -0,0 +1,18 @@
+// const
+
+const a = 10;
+console.log(a);
+// undefined 
+const b;
+console.log(b);
+
+//redeclara
+// const x = 10;
+// const x =15;
+// const x =123;
+// console.log(x);
+//reassign 
+// const y = 12;
+// y = 15;
+// y = 40;
+// as you can in const but redeclare and reassign can't work 
---
+++ javascript/var.js
@@ -0,0 +1,20 @@
+// var 
+
+var a = 150;
+console.log(a);
+
+// undefined 
+var b;
+console.log(b);
+
+// redeclare
+var x = 12;
+var x = 15;
+var x = 20;
+console.log(x);
+
+// reassign
+var y = 10;
+y = 20;
+y = 30;
+console.log(y);
